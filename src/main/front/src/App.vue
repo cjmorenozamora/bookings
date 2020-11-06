5698ad4e48f359af7d092ec418689db89b563ec2
cjmorenozamora <cris.moreno.zamora@gmail.com>
2020-11-06 22:57:00 +0100

    Creación del menú

--- src/main/front/src/App.vue
+++ src/main/front/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app" class="hello">
+    <slider :width="300" format="push" direction="left" :opacity="0.15" :links="[{'id': 1, 'text': 'Mostrar Hoteles', 'url': 'https://github.com'}, {'id': 2, 'text': 'Haz una reserva', 'url': 'https://github.com'}, {'id': 2, 'text': 'Consultar Reservas', 'url': 'https://github.com'}]"></slider>
     <img alt="Vue logo" src="./assets/NOSOLOSOFTWARE.png">
     <HelloWorld msg="Reserva de Hotel"/>
     <hr>
@@ -10,12 +11,13 @@
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import GetHotels from './components/GetHotels.vue'
-
+import Slider from '@jeremyhamm/vue-slider'
 export default {
   name: 'App',
   components: {
     HelloWorld,
-    GetHotels
+    GetHotels,
+    'slider': Slider
   }
 }
 </script>
